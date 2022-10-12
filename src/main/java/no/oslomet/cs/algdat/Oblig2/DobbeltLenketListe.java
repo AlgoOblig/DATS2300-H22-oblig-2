4503048eaab922b1d77ac7b1fd1d3c611706f4ea
AlgoOblig <gitalgoritm@gmail.com>
2022-10-12 19:51:19 +0200

    Main metod er fjernet fra DoubletLenkliste klass

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -465,49 +465,6 @@ public class DobbeltLenketListe<T> implements Liste<T> {
     }
 
 
-    public static void main(String[] args) {
-
-
-        Character[] c = {'A','B','C','D','E','F','G','H','I','J',};
-        DobbeltLenketListe<Character> liste = new DobbeltLenketListe<>(c);
-        System.out.println(liste.subliste(3,8)); // [D, E, F, G, H]
-        System.out.println(liste.subliste(5,5)); // []
-        System.out.println(liste.subliste(8,liste.antall())); // [I, J]
-// System.out.println(liste.subliste(0,11)); // skal kaste unntak
-
-
-      /*  String[] s1 = {}, s2 = {"A"}, s3 = {null,"A",null,"B",null};
-        DobbeltLenketListe<String> l1 = new DobbeltLenketListe<>(s1);
-        DobbeltLenketListe<String> l2 = new DobbeltLenketListe<>(s2);
-        DobbeltLenketListe<String> l3 = new DobbeltLenketListe<>(s3);
-        System.out.println(l1.toString() + " " + l2.toString()
-                + " " + l3.toString() );*/
-
-
-
-       /** String[] s1 = {}, s2 = {"A"}, s3 = {null,"A",null,"B",null};
-        DobbeltLenketListe<String> l1 = new DobbeltLenketListe<>(s1);
-        DobbeltLenketListe<String> l2 = new DobbeltLenketListe<>(s2);
-        DobbeltLenketListe<String> l3 = new DobbeltLenketListe<>(s3);
-        System.out.println(l1.toString() + " " + l2.toString()
-                + " " + l3.toString() + " " + l1.omvendtString() + " "
-                + l2.omvendtString() + " " + l3.omvendtString());
-
-
-        DobbeltLenketListe<Integer> liste = new DobbeltLenketListe<>();
-        System.out.println(liste.toString() + " " + liste.omvendtString());
-        for (int i = 1; i <= 3; i++) {
-            liste.leggInn(i);
-            System.out.println(liste.toString() + " " + liste.omvendtString());
-        }
-// Utskrift:
-// [] []
-// [1] [1]
-// [1, 2] [2, 1]
-// [1, 2, 3] [3, 2, 1]
-
-*/
-    }
 
 } // class DobbeltLenketListe
 
